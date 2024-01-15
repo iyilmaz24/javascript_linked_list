12cabb5f7ecea9c754939ecadebe12014549f5b1
Irfan Yilmaz <iy.usa@outlook.com>
2024-01-15 17:12:56 -0500

    add L-List to string method, add L-List prepend method

--- LinkedList.js
+++ LinkedList.js
@@ -16,7 +16,24 @@ export default class LinkedList {
         else {
             console.log("List currently empty!");
         }
+    }
 
+    ToString() {
+        let stringRepresentation = "";
+
+        if(this.length == 0) {
+            stringRepresentation = 'null';
+        }
+        else{
+            let temp = this.head;
+            for(let i = 0; i < this.length; i++) {
+                stringRepresentation += `( ${temp.data} ) -> `
+                temp = temp.next;
+            }
+            stringRepresentation += 'null';
+        }
+
+        console.log(stringRepresentation);
     }
 
     Append(node) {
@@ -29,6 +46,31 @@ export default class LinkedList {
             this.tail = node;
         }
         this.length++;
+    }
+
+    Prepend(node) {
+        if(!this.head) {
+            this.head = node;
+            if(!this.tail) {
+                this.tail = node;
+            }
+        }
+        else {
+            node.next = this.head
+            this.head = node;
+        }
+    }
+
+    Pop() {
+        console.log("Remove the last element from the list");
+    }
+
+    LookUp(index) {
+        console.log("Return the node at the given index");
+    }
+
+    Contains(value) {
+        console.log("Return index of given value if valid else false");
     }
 
     InsertAt(index) {
--- Main.js
+++ Main.js
@@ -2,11 +2,16 @@ import Node from './Node.js';
 import LinkedList from './LinkedList.js';
 
 
-const demoNode = new Node(11);
-demoNode.Print();
+const demoNode11 = new Node(11);
+demoNode11.Print();
+
+const demoNode99 = new Node(99);
+demoNode99.Print();
 
 const myList = new LinkedList();
 myList.Print();
-myList.Append(demoNode);
+myList.Append(demoNode11);
+myList.Append(demoNode99);
 myList.Print();
 
+myList.ToString();
